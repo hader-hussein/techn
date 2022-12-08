dd47f3c33df76121259da39ecaa97994ae41a584
hader-hussein <91197408+hader-hussein@users.noreply.github.com>
2022-12-08 15:30:20 +0200

    first commit

---
+++ assets/js/form.js
@@ -0,0 +1,179 @@
+//conent-us//
+(() => {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    const forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+      form.addEventListener('submit', event => {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+  
+        form.classList.add('was-validated')
+      }, false)
+    })
+  })()
+
+
+//activ-link-nav//
+const currentlocation = location.href; 
+const menuitem = document.querySelectorAll('.navber-index .nav-item .nav-link,.component-link-Pages a,.nav-pills li a');
+ const menulength = menuitem.length
+for (let i = 0; i<menulength; i++){
+ if(menuitem[i].href === currentlocation) {
+   menuitem[i].className = "active" 
+    }
+}
+const currentabout = location.href; 
+const menuitemabout = document.querySelectorAll('.nav-about ol li a');
+ const menulengthabout = menuitemabout.length
+for (let j = 0; j<menulengthabout; j++){
+ if(menuitemabout[j].href === currentabout) {
+  menuitemabout[j].className = "active" 
+    }
+}
+
+//activ-link-nav//
+function toggleText(){
+  $('.btn-colik').toggleClass('transform-active');
+  var x = document.getElementsByClassName("overlay-caption");
+  var m = document.getElementsByClassName("carousel-caption-one");
+  Array.from(x).forEach((xl) => {
+    if ((xl.style.display == 'none')) {
+      xl.style.display = "block";
+      
+  
+    } else {
+      xl.style.display = "none";
+      
+    }
+  })
+  Array.from(m).forEach((ml) => {
+    if ((ml.style.display == 'none')) {
+      ml.style.display = "block";
+      
+  
+    } else {
+      ml.style.display = "none";
+      
+    }
+  })
+ 
+}
+// ******************VIDEO*****************************
+
+var myVideo = document.getElementById('myVideo');
+
+$(".video-date").on('click',function () {
+    // debugger;
+        if (myVideo.paused){
+        $("#myBtn").hide();
+        myVideo.play();
+       
+        } 
+        else {
+        myVideo.pause();
+        $("#myBtn").show();
+        }
+    });
+/**** */
+let items = document.querySelectorAll('.carousel-Offers .carousel-offers-item')
+
+items.forEach((el) => {
+    const minPerSlide = 2
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
+$(window).scroll(function(){
+  $(' .navber-index').toggleClass('scrolled', $(this).scrollTop() > 50);
+  });
+  $(document).ready(function() {
+
+    $(".component-team-new").owlCarousel(
+      {
+        navText: ["<img src='./assets/img/Iconleft.png'>","<img src='./assets/img/Iconright.png'>"],
+        navClass:['owl-prev','owl-next'],
+        ltr: true,
+      
+        autoplay:true,
+        autoplayhoverpause: true,
+        autoplaytimeout:100,
+        items:2,
+        nav:true,
+        responsive:{
+          0:{
+            items:1,
+            dots:false
+          },
+          485:{
+            items:2,
+            dots:false
+          },
+          728:{
+            items:2,
+            dots:false
+          },
+          960:{
+            items:2,
+            dots:false
+          },
+          1200:{
+            items:3,
+            dots:true
+          },
+        }
+      }
+    );
+    $(".component-team-Project").owlCarousel(
+      {
+        navText: ["<img src='./assets/img/icon-left.svg'>","<img src='./assets/img/icon-right.svg'>"],
+        navClass:['owl-prev','owl-next'],
+        ltr: true,
+        autoplay:true,
+        autoplayhoverpause: true,
+        autoplaytimeout:100,
+        items:2,
+        nav:true,
+        responsive:{
+          0:{
+            items:1,
+            dots:false
+          },
+          485:{
+            items:2,
+            dots:false
+          },
+          728:{
+            items:2,
+            dots:false
+          },
+          960:{
+            items:2,
+            dots:false
+          },
+          1200:{
+            items:3,
+            dots:true
+          },
+        }
+      }
+    );
+   
+  })
+  /*** */
+  /*active*/ 
+  $('.navber-index .nav-item .nav-link').removeClass('active').removeAttr('aria-current');
+  $('a[href="' + location.pathname + '"]').closest('li').addClass('active').attr('aria-current', 'page'); 
+  /*ative*/
